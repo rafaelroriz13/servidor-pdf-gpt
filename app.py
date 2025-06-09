cb25ead722c4e3375a23a412fd15a510d92583db
Rafael Roriz <rafaelroriz13@hotmail.com>
2025-06-09 11:44:05 -0300

    Corrige porta e host para deploy no Render

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def serve_openapi():
     return send_from_directory('.', 'openapi.yaml', mimetype='text/yaml')
 
 if __name__ == '__main__':
-    app.run(port=5000)
+  app.run(host="0.0.0.0", port=10000)
+
